4451b20acc90174b6a4a3b8aa6c3b55e604998b3
cs-cognitome <gaya.scienza.v@gmail.com>
2019-12-23 17:57:15 +0200

    Updates

---
+++ precache-manifest.535a7de9ab70a514ebd036284bb2b061.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5edb7b45734946d0190d31e003f12164",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "57a2dda0e9c224b76a9c",
+    "url": "/react_dynamic-list-of-posts/static/css/main.52af84ab.chunk.css"
+  },
+  {
+    "revision": "a93dcb5f4871ee723658",
+    "url": "/react_dynamic-list-of-posts/static/js/2.a2bac020.chunk.js"
+  },
+  {
+    "revision": "57a2dda0e9c224b76a9c",
+    "url": "/react_dynamic-list-of-posts/static/js/main.cc6ce112.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
